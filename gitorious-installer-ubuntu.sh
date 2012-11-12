92c36084e3d07a0ed35eb34c1edca7e714667590
celogeek <me@celogeek.com>
2012-11-12 20:50:34 +0100

    check debootstrap with lucid support

--- gitorious-installer-ubuntu.sh
+++ gitorious-installer-ubuntu.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+#CHECK NEED PACKAGE
+DBS=$(which debootstrap)
+if [ "$DBS" == "" ] || [ ! -e /usr/share/debootstrap/scripts/lucid ]
+then
+	echo "Please install debootstrap with Ubuntu Lucid support"
+	exit 1
+fi
